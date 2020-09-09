f79a7aca255fe8954b8be314b9494d2025cc293a
vlad <vl.hutsal@gmail.com>
2020-09-10 01:29:15 +0300

    add comments field on topic page add topic related comments on topic page

--- homepage/forms.py
+++ homepage/forms.py
@@ -2,3 +2,11 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+from django.forms import ModelForm
+
+from homepage.models import Topic, Comment, Tag
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']

e6b916901d9c35266638cfeb75d224ec09794a05
vlad <vl.hutsal@gmail.com>
2020-09-27 02:45:10 +0300

    add: delete comment function add: api func hello world add: title inheritance with block.super Django filter add: get absolute url method for Topic model rename: funcs on urls.py home -> home_page, topic -> topic_page add: add topic func separated from home_page for readable rename: test page to myHut

--- homepage/views.py
+++ homepage/views.py
@@ -2,27 +2,20 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.models import User, Permission
 from django.contrib.contenttypes.models import ContentType
 
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.db.models import Count
 
 from homepage.models import Topic, Comment, Tag
 from homepage.forms import CommentForm, TopicForm
 
-# return render(request, 'homepage/error.html', {'error': })
-
 # add topic desription for 300 symbols
 
-# Page views
+# Read views
 def home_page(request):
     if request.user.is_authenticated is False:
         return redirect('login:user_login')
     else:
-        form = TopicForm(request.POST or None)
-        if form.is_valid():
-            topic = form.save(commit=False)
-            topic.user = User.objects.get(username=request.user)
-            topic.save()
-            return redirect('homepage:home')
+        form = TopicForm(None)
         annotated_topic_queryset = Topic.objects.annotate(comm_count=Count('comment'))
         context = {
             'name': request.user,
@@ -31,13 +24,11 @@ def home_page(request):
             }
         return render(request, 'homepage/home_page.html', context)
 
-
 def topic_page(request, topic_id):
     topic_object = Topic.objects.get(pk=topic_id)
     topic_comments = Comment.objects.filter(topic=topic_object)
     user = request.user
     has_delete_permition = user.has_perm('homepage.delete_comment')
-
     context = {
         'form': CommentForm,
         'topic_object': topic_object,
@@ -47,28 +38,38 @@ def topic_page(request, topic_id):
     return render(request, 'homepage/topics_page.html', context)
 
 
-def test(request):
-    return render(request, 'homepage/test.html')
+def myHut(request):
+    return render(request, 'homepage/user_room.html')
 
 
-
-# Comment views
+# Create views
 def add_comment(request, topic_id):
     if request.method == "POST":
         user = request.user
         text = request.POST.get('text')
         topic = Topic.objects.get(pk=topic_id)
         Comment.objects.create(user=user, text=text, topic=topic)
-    return redirect('homepage:topic', topic_id=topic_id)
+    return redirect('homepage:topic_page', topic_id=topic_id)
+
+def addTopic(request):
+    form = TopicForm(request.POST)
+    if form.is_valid():
+        topic = form.save(commit=False)
+        topic.user = User.objects.get(username=request.user)
+        topic.save()
+        return redirect('homepage:topic_page', topic_id=topic.id)
+    return redirect('homepage:home_page')
 
 
-def delete_comment(request, comment_id):
-    current_comment = Comment.objects.get(id=comment_id)
+# Delete views
+def delete_comment(request, comment_pk):
+    current_comment = Comment.objects.get(pk=comment_pk)
     current_comment.delete()
     topic_id = current_comment.topic.id
-    return redirect('homepage:topic', topic_id=topic_id)
+    return redirect('homepage:topic_page', topic_id=topic_id)
 
 
+# Update views
 def delete_comment_permission(request, topic_id):
     content_type = ContentType.objects.get_for_model(Comment)
     permission = Permission.objects.get(
@@ -77,15 +78,29 @@ def delete_comment_permission(request, topic_id):
         )
     user = request.user
     user.user_permissions.add(permission)
-    return redirect('homepage:topic', topic_id=topic_id)
+    return redirect('homepage:topic_page', topic_id=topic_id)
+
 
 def comment_rating_change(request, topic_id, comment_id):
     comment = Comment.objects.get(pk=comment_id)
-    print(request.POST)
+    user = request.user
     if 'up' in request.POST:
-        comment.rating += 1
-        comment.save()
+        if user in comment.likes.all():
+            pass
+        else:
+            comment.likes.add(user)
     elif 'down' in request.POST:
-        comment.rating -= 1
-        comment.save()
-    return redirect('homepage:topic', topic_id=topic_id)
+        if user in comment.likes.all():
+            comment.likes.remove(user)
+        else:
+            pass
+    return redirect('homepage:topic_page', topic_id=topic_id)
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view()
+def hello_world(request):
+    return Response({
+        'message': 'Hello world'
+    })

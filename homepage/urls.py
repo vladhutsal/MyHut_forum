8e499a9a6ef7c5c59bc77544cbc783408c041c5c
vlad <vl.hutsal@gmail.com>
2020-10-31 21:16:22 +0200

    add url for api topic creation

--- homepage/urls.py
+++ homepage/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path("topic/<slug:slug>/", views.topic_page, name="topic_page"),
 
     path("add_comment/<slug:slug>/", views.add_comment, name="add_comment"),
-    path("add_topic", views.add_topic, name="add_topic"),
 
     path("delete_comment/<int:comment_pk>/", views.delete_comment, name="delete_comment"),
     path('gain_delete_permission/<slug:slug>/', views.gain_delete_permission, name="gain_delete_perm")

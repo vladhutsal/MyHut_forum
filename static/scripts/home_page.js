64f9464258cec141ff9ab52ecc6c586e879208cf
vlad <vl.hutsal@gmail.com>
2020-11-19 18:04:49 +0200

    update: createTopic function now sends form data via handleRequest properly. add: getCookie function to get CSRF token and use it as a header, while sending data using fetch()

--- static/scripts/home_page.js
+++ static/scripts/home_page.js
@@ -6,7 +6,7 @@ getTopicsList(topicsContainer)
 
 
 function getTopicsList(topicsEl) {
-  handleRequest('api_topics/').then((data) => {
+  handleRequest('api/topics/').then((data) => {
     console.log(data)
 
     let finalTopic = "";
@@ -22,9 +22,11 @@ function getTopicsList(topicsEl) {
   return;
 }
 
+
 // lets make requests with fetch method, not xhr
-async function handleRequest(url, method, data) {
-    const res = await fetch(url, method, data);
+async function handleRequest(url, data) {
+    console.log(data)
+    const res = await fetch(url, data);
     const responseData = await res.json();
     return responseData;
 }
@@ -32,7 +34,7 @@ async function handleRequest(url, method, data) {
 
 generateTopicEl = (element) => {
   var generatedTopicEl = "<div class='col-12 mx-auto border py-3 mb-4'>\
-  <h4><a href='topic/" + element.slug + "'>" + element.title + "</a></h4>\
+  <h4><a href='topic/" + element.id + "'>" + element.title + "</a></h4>\
   <p>" + element.text + "</p>\
   </div>"
   return generatedTopicEl
@@ -41,13 +43,21 @@ generateTopicEl = (element) => {
 
 function createTopic(event) {
   event.preventDefault();
-  const topicForm = event.target;
-  const newFormData = new FormData(topicForm);
+  const newFormData = new FormData(event.target);
+  const data = {
+    title: newFormData.get('title'),
+    text: newFormData.get('text')
+  }
 
-  handleRequest('api_topics/create', 'POST', newFormData)
-    .then(response => {
-        const status = response.status;
-        const data = response.response;
+  handleRequest('api/topics/create', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json',
+      'X-CSRFToken': getCookie('csrftoken')
+    },
+  }).then(response => {
+        console.log(response)
 
   // early return method, easier to look at the code
   if (![201, 200].includes(status)) {
@@ -64,4 +74,20 @@ function createTopic(event) {
 
   topicForm.reset();
   })
+}
+
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
 }
